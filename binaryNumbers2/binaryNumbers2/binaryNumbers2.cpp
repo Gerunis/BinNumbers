553c564343a2ab8d674c1dc757e8c161038b3db2
Сергей Русинов <fftron@me.com>
2019-09-20 23:10:13 +0500

    Ввод/вывод

--- binaryNumbers2/binaryNumbers2/binaryNumbers2.cpp
+++ binaryNumbers2/binaryNumbers2/binaryNumbers2.cpp
@@ -9,30 +9,20 @@
 
 using namespace std;
 
-bitset<32>& Sum(bitset<32> & a, bitset<32> & b) {
-	bitset<32> res;
-	int c = 0, n, p;
-	for (int i = 0; i < 32; i++) {
-		p = a[i] + b[i] + c;
-		n = (p) % 2;
-		res[i] = n;
-		c = (p) / 2;
-	}
-	return res;
-}
-
-bitset<32>& ToTwo(int a)
+bitset<32> & toInt(int number)
 {
-	cout << bitset<32>(a) << endl;
 	bitset<32> res;
 	int k;
-	bool flag = false, minus = a < 0;
-	if (minus) res = ~res;
-	int b = abs(a);
+	bool flag = false, negative = false;
+	if (number < 0) {
+		negative = true;
+		res = ~res;
+		number *= -1;
+	}
 
-	for (int i = 0; b != 0 && i < 32; i++) {
-		k = b % 2;
-		if (minus) {
+	for (int i = 0; number != 0 && i < 32; i++) {
+		k = number % 2;
+		if (negative) {
 			if (flag) {
 				k = (k + 1) % 2;
 			}
@@ -41,159 +31,197 @@ bitset<32>& ToTwo(int a)
 			}
 		}
 		res[i] = k;
-		b = b / 2;
+		number /= 2;
 	}
 
 	return res;
 }
 
-bitset<32> toFloat(float a) {
+bitset<32>& sumInt(bitset<32> & a, bitset<32> & b) {
+	bitset<32> res;
+	int c = 0, n, p;
+	for (int i = 0; i < 32; i++) {
+		p = a[i] + b[i] + c;
+		n = p % 2;
+		res[i] = n;
+		c = p / 2;
+	}
+	return res;
+}
+
+bitset<32> toFloat(float number) {
 
 	bitset<32> res;
 	bool flag = false;
 
-	if (a < 0) {
+	if (number < 0) {
 		res[31] = 1;
-		a *= -1;
+		number *= -1;
 	}
 	int p = 127;
-	int t = 0;
-	int e = 128;
-	float q = pow(2, p);
+	int mantissaCurrentSize = 0;
+	int exponent = 128;
+	float q = pow(2, p); 
 
-	while (t <= 23 && p >= 0) {
-		if (a >= q) {
+	while (mantissaCurrentSize <= 23 && p >= 0) {
+		if (number >= q) {
 			if (flag)
 			{
-				res[23 - t] = 1;
+				res[23 - mantissaCurrentSize] = 1;
 			}
 			else
 			{
 				flag = true;
-				e = p;
+				exponent = p;
 			}
-			a -= q;
+			number -= q;
 		}
 
-		if (flag) t++;
+		if (flag) mantissaCurrentSize++;
 		q /= 2;
 		p--;
 	}
-	while (t <= 23 && p >= -127) {
-		a *= 2;
-		if (a >= 1) {
-			a -= 1;
+
+	while (mantissaCurrentSize <= 23 && p >= -127) {
+		number *= 2;
+		if (number >= 1) {
+			number -= 1;
 			if (flag) {
-				res[23 - t] = 1;
+				res[23 - mantissaCurrentSize] = 1;
 			}
 			else
 			{
 				flag = true;
-				e = p;
+				exponent = p;
 			}
 		}
-		if (flag) t++;
+		if (flag) mantissaCurrentSize++;
 		p--;
 	}
-	if (e == 128) return res;
-	e += 127;
+	if (exponent == 128) return res;
+
+	exponent += 127;
 	for (int i = 0; i < 8; i++) {
-		res[i + 23] = e % 2;
-		e /= 2;
+		res[i + 23] = exponent % 2;
+		exponent /= 2;
 	}
 
 	return res;
 }
 
-int TakeSub(bitset<32> & biger, bitset<32> lower) {
+int getSub(bitset<32> & greaterNumber, bitset<32> lowerNumber) {
 	int sub = 0, k = 0, n = 1;
 	for (int i = 23; i < 31; i++) {
-		if (biger[i] == 1) {
-			if (lower[i] == k)
+		if (greaterNumber[i] == 1) {
+			if (lowerNumber[i] == k)
 				sub += n;
 			else k = 0;
 		}
 		else {
-			if (lower[i] != k) {
+			if (lowerNumber[i] != k) {
 				sub += n;
 				k = 1;
 			}
 		}
 		n *= 2;
 	}
-	cout << sub << endl;
 	return sub;
 }
 
-bitset<32> & SumMantisses(bitset<32> &big, bitset<32> &small, int sub) {
+bitset<32> & sumMantiss(bitset<32> &greaterNumber, bitset<32> &lowerNumber, int sub) {
 	int n = 0, p, i;
 	bitset<32> res;
 	for (i = 0; i <= 23 - sub; i++) {
 
-		p = (i == 23 ? 1 : big[i]) + ((i+sub) == 23 ? 1 : small[i + sub]) + n;
+		p = (i == 23 ? 1 : greaterNumber[i]) + ((i+sub) == 23 ? 1 : lowerNumber[i + sub]) + n;
 		res[i] = p % 2;
 		n = p / 2;
 	}
+
 	for (; i < 23; i++) {
-		p = big[i] + n;
+		p = greaterNumber[i] + n;
 		res[i] = p % 2;
 		n = p / 2;
 	}
+
 	if (n == 1) {
 		for (i = 1; i < 24; i++) {
 			res[i - 1] = res[i];
 		}
+		for (int i = 23; i < 31; i++) {
+			p = greaterNumber[i] + n;
+			res[i] = p % 2;
+			n = p / 2;
+		}
 	}
-
-	for (int i = 23; i < 31; i++) {
-		p = big[i] + n;
-		res[i] = p % 2;
-		n = p / 2;
+	else {
+		for (int i = 23; i < 31; i++) {
+			res[i] = greaterNumber[i];
+		}
 	}
+	
 	return res;
 }
 
-bitset<32>& SumFloat(bitset<32> & a, bitset<32> & b) {
-	int sub = 0, n = 0, p;
+bitset<32>& sumFloat(bitset<32> & a, bitset<32> & b) {
+	int sub = 0;
 	for (int i = 30; i >= 23; i--) {
 		if (a[i] > b[i]) {
-			sub = TakeSub(a, b);
-			return SumMantisses(a, b, sub);
+			sub = getSub(a, b);
+			return sumMantiss(a, b, sub);
 		}
 		if (a[i] < b[i]) {
-			sub = TakeSub(b, a);
-			return SumMantisses(b, a, sub);
+			sub = getSub(b, a);
+			return sumMantiss(b, a, sub);
 		}
 	}
-	return SumMantisses(a, b, 0);
+	return sumMantiss(a, b, sub);
 }
-
-
-
 
 int main()
 {
-	float a = 176.0918;
-	float b = 0.48914984984189;
+	int a, b, c;
+	cout << "First integer number: ";
+	cin >> a;
+	bitset<32> binaryA = toInt(a);
+	cout << "Reference    : " << bitset<32>(a) << endl;
+	cout << "Code variant : " << binaryA << endl;
+	cout << "Second integer number: ";
+	cin >> b;
+	bitset<32> binaryB = toInt(b);
+	cout << "Reference    : " << bitset<32>(b) << endl;
+	cout << "Code variant : " << binaryB << endl;
 
-	//float a = 372.1;
-	//float b = 25.567;
-	int* rf = reinterpret_cast<int*>(&a);
-	cout << bitset<32>(*rf) <<" "<< a << endl;
-	rf = reinterpret_cast<int*>(&b);
-	cout << bitset<32>(*rf) << " " << b << endl;
+	c = a + b;
+	bitset<32> binaryC = sumInt(binaryA, binaryB);
+	cout << "Sum: " << a << " + " << b << " = " << c << endl;
+	cout << "Reference    : " << bitset<32>(c) << endl;
+	cout << "Code variant : " << binaryC << endl;
 
-	cout << endl;
-
-	bitset<32> bsa = toFloat(a);
-	bitset<32> bsb = toFloat(b);
-	bitset<32> bsc = SumFloat(bsa, bsb);
-
-	float c = a + b;
 	
-	rf = reinterpret_cast<int*>(&c);
-	cout << bitset<32>(*rf) << endl;
-	cout << bsc << endl;
+	float d, e, f;
+
+	cout << "First fractional number: ";
+	cin >> d;
+	int* rf = reinterpret_cast<int*>(&d);
+	bitset<32> binaryD = toFloat(d);
+	cout << "Reference    : " << bitset<32>(*rf) << endl;
+	cout << "Code variant : " << binaryD << endl;
+
+	cout << "Second fractional number: ";
+	cin >> e;
+	rf = reinterpret_cast<int*>(&e);
+	bitset<32> binaryE = toFloat(e);
+	cout << "Reference    : " << bitset<32>(*rf) << endl;
+	cout << "Code variant : " << binaryE << endl;
+
+	f = d + e;
+	cout << "Sum: " << d << " + " << e << " = " << f << endl;
+	rf = reinterpret_cast<int*>(&f);
+	bitset<32> binaryF = sumFloat(binaryD, binaryE);
+	cout << "Reference    : " << bitset<32>(*rf) << endl;
+	cout << "Code variant : " << binaryF << endl;
+
 	return 0;
 }
 
